558cba051bd4f04a3aa11d70a837762e0743875e
Rmolimock <495@holbertonschool.com>
2021-02-17 14:59:53 +0000

    fixed url for test api calls

--- test_api.py
+++ test_api.py
@@ -1,12 +1,13 @@
 import unittest
 import requests
 from flask import url_for
+from helpers import build_url
 
 class TestAPI(unittest.TestCase):
     def test_get_all_projects_status(self):
-        r = requests.get('127.0.0.1:8080/api/projects').json()
+        r = requests.get(build_url('/api/projects')).json()
         self.assertEqual(r.get('status'), 'OK')
 
     def test_get_project_by_name(self):
-        r = requests.get('127.0.0.1:8080/api/projects/name/cheese').json()
+        r = requests.get(build_url('/api/projects/name/cheese')).json()
         self.assertEqual(r.get('status'), 'OK')
